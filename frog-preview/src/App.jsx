cc5762691cc7d177eab558ac44e160e0da36530a
shazacollege03 <shazacollege03@gmail.com>
2025-12-30 14:57:55 +0300

    frog hop and envelope push fixed

--- frog-preview/src/App.jsx
+++ frog-preview/src/App.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import Frog from "./Frog";
 import Envelope from "./Envelope";
+import EnvelopeOpen from "./EnvelopeOpen";
 import "./style.css";
 
 export default function App() {
   return (
     <div className="app">
-      <div style={{ width: "400px", height: "400px" }}>
-        <Frog className="frog-hop" />
-        <Envelope className="envelope-float"/>
+        <div className="scene">
+          <Frog  />
+          <Envelope />
+          <EnvelopeOpen className="envelope-float" />
+        </div>
       </div>
-    </div>
   );
 }
