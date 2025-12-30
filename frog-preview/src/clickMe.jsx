24e8446cce5852e8967368f232f322d1e6086329
shazacollege03 <shazacollege03@gmail.com>
2025-12-31 00:29:48 +0300

    Deploy React app to GitHub Pages

--- frog-preview/src/clickMe.jsx
+++
@@ -1,5 +0,0 @@
-export default function ClickMe() {
-    return(
-        <div className="floating-text pixelify-sans-fontt">Click Me!</div>
-    );
-}
--- frog-preview/src/clickmeAgain.jsx
+++
@@ -1,7 +0,0 @@
-export default function ClickMeAgain() {
-    return(
-        <div className="clickagain floating-text pixelify-sans-fontt">
-            Click Me Again ^-^
-            </div>
-    );
-}
