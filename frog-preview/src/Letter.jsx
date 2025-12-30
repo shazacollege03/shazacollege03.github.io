3d1217235fa73f00ae30a32aedcfb91aed7ee69a
shazacollege03 <shazacollege03@gmail.com>
2025-12-30 17:25:23 +0300

    buttons adjusted

--- frog-preview/src/Letter.jsx
+++ frog-preview/src/Letter.jsx
@@ -1,36 +1,42 @@
 import { useState } from "react";
+import leftButton from "/src/assets/left-button.svg";
+import rightButton from "/src/assets/right-button.svg";
 
 const pages = [
-  "/src/assets/letter/pg1.JPG",
-  "/src/assets/letter/pg2.JPG",
-  "/src/assets/letter/pg3.JPG",
-  "/src/assets/letter/pg4.JPG",
+    "/src/assets/letter/pg1.JPG",
+    "/src/assets/letter/pg2.JPG",
+    "/src/assets/letter/pg3.JPG",
+    "/src/assets/letter/pg4.JPG",
 ];
 
 export default function LetterViewer() {
-  const [page, setPage] = useState(0);
+    const [page, setPage] = useState(0);
 
-  return (
-    <div className="letter-viewer">
-      <img src={pages[page]} alt={`Letter page ${page + 1}`} />
+    return (
+        <div className="letter-viewer">
+            <img src={pages[page]} alt={`Letter page ${page + 1}`} />
 
-      <div className="letter-controls">
-        <button
-          onClick={() => setPage(p => Math.max(0, p - 1))}
-          disabled={page === 0}
-        >
-          ◀
-        </button>
+            <div className="letter-controls">
+                <button
+                    className="icon-button"
+                    onClick={() => setPage(p => Math.max(0, p - 1))}
+                    disabled={page === 0}
+                >
+                    <img src={leftButton} alt="Previous page" />
+                </button>
 
-        <button
-          onClick={() =>
-            setPage(p => Math.min(pages.length - 1, p + 1))
-          }
-          disabled={page === pages.length - 1}
-        >
-          ▶
-        </button>
-      </div>
-    </div>
-  );
+
+                <button
+                    className="icon-button"
+                    onClick={() =>
+                        setPage(p => Math.min(pages.length - 1, p + 1))
+                    }
+                    disabled={page === pages.length - 1}
+                >
+                    <img src={rightButton} alt="Next page" />
+                </button>
+
+            </div>
+        </div>
+    );
 }
